cf19509fb693acc6ce8ba320735d49ba98870efa
danmil5 <dmmiller1361@eagle.fgcu.edu>
2019-09-04 18:57:25 -0400

    initial commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,7 +12,22 @@ public class Controller {
 
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
         if (user.getText().equals("admin") && pass.getText().equals("password")) {
-            actiontarget.setText("Login Successful");
+            actiontarget.setText("Administrator Login Successful");
+        }
+        else if (user.getText().equals("Emma") && pass.getText().equals("Jones")) {
+            actiontarget.setText("Welcome back emma.jones@example.com");
+        }
+        else if (user.getText().equals("Emma") && pass.getText().equals("Jones")) {
+            actiontarget.setText("Welcome back ethan.william@example.com");
+        }
+        else if (user.getText().equals("Isabella") && pass.getText().equals("Johnson")) {
+            actiontarget.setText("Welcome back isabella.johnson@example.com");
+        }
+        else if (user.getText().equals("Jacob") && pass.getText().equals("Smith")) {
+            actiontarget.setText("Welcome back jacob.smith@example.com");
+        }
+        else if (user.getText().equals("Michael") && pass.getText().equals("Brown")) {
+            actiontarget.setText("Welcome back michael.brown@example.com");
         }
         else {
             actiontarget.setText("Invalid Login Credentials");
